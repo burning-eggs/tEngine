ec07da54a11fd07d457092f6f4b287d7a932c6a0
flying-dojo <pro-uno-player@protonmail.com>
2021-07-08 19:44:51 +0300

    Reorganize

--- ts/app.ts
+++ ts/app.ts
@@ -1,25 +1,3 @@
-namespace TE {
-    export class Engine {
-        private _count: number = 0
-
-        public constructor() {
-
-        }
-
-        public start(): void {
-            this.loop()
-        }
-
-        private loop(): void {
-            this._count++;
-
-            document.title = this._count.toString()
-
-            requestAnimationFrame(this.loop.bind(this))
-        }
-    }
-}
-
 window.onload = function() {
     let engine = new TE.Engine()
 
---
+++ ts/engine.ts
@@ -0,0 +1,21 @@
+namespace TE {
+    export class Engine {
+        private _count: number = 0
+
+        public constructor() {
+
+        }
+
+        public start(): void {
+            this.loop()
+        }
+
+        private loop(): void {
+            this._count++;
+
+            document.title = this._count.toString()
+
+            requestAnimationFrame(this.loop.bind(this))
+        }
+    }
+}
