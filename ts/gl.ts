4fb635abe4c4478397a428c18c73dabc592edd4d
flying-dojo <pro-uno-player@protonmail.com>
2021-07-08 20:54:15 +0300

    Canvas locating 101

--- ts/gl.ts
+++ ts/gl.ts
@@ -1,8 +1,19 @@
 namespace TE {
+    /**
+     * WebGL Context
+     */
     export var gl: WebGLRenderingContext
 
+    /**
+     * Responsible for creating WebGL context.
+     */
     export class GLUtilities {
-        public static initialize(elementId?: string): void {
+        /**
+         * Initializes WebGL, potentially using the canvas with an assigned ID if it is defined.
+         * @param elementId The ID of the element to search for
+         * @returns
+         */
+        public static initialize(elementId?: string): HTMLCanvasElement {
             let canvas: HTMLCanvasElement
 
             if (elementId !== undefined) {
@@ -13,7 +24,17 @@ namespace TE {
                 }
             } else {
                 canvas = document.createElement("canvas") as HTMLCanvasElement
+
+                document.body.appendChild(canvas)
             }
+
+            gl = canvas.getContext("webgl")
+
+            if (gl === undefined) {
+                throw new Error("Unable to initialize WebGL.")
+            }
+
+            return canvas
         }
     }
 }
