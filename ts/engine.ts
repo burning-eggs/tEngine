dad83a2f8b231e153426c443d7c1f91e5947328b
flying-dojo <pro-uno-player@protonmail.com>
2021-07-08 20:16:45 +0300

    Add documentation

--- ts/engine.ts
+++ ts/engine.ts
@@ -1,15 +1,28 @@
 namespace TE {
+
+    /**
+     * Main engine class.
+     */
     export class Engine {
         private _count: number = 0
 
+        /**
+         * Creates a new engine.
+         */
         public constructor() {
 
         }
 
+        /**
+         * Starts up the engine.
+         */
         public start(): void {
             this.loop()
         }
 
+        /**
+         * Main loop for the engine.
+         */
         private loop(): void {
             this._count++;
 
