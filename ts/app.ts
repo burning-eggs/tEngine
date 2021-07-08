57281569ca5c470a491f4ac78b66d72f45e69eab
flying-dojo <pro-uno-player@protonmail.com>
2021-07-08 19:40:32 +0300

    Jumpstart tEngine

--- ts/app.ts
+++ ts/app.ts
@@ -1,11 +1,27 @@
 namespace TE {
     export class Engine {
+        private _count: number = 0
+
         public constructor() {
-            console.log("tEngine FTW?????")
+
+        }
+
+        public start(): void {
+            this.loop()
+        }
+
+        private loop(): void {
+            this._count++;
+
+            document.title = this._count.toString()
+
+            requestAnimationFrame(this.loop.bind(this))
         }
     }
 }
 
 window.onload = function() {
-    let engine = new TE.Engine
+    let engine = new TE.Engine()
+
+    engine.start()
 }
